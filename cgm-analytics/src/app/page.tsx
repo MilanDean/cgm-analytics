dfc22566c389ad7237c4dfa7f969a39266a8bcfe
MilanDean <milan387@gmail.com>
2023-05-29 00:35:01 -0500

    modified bear image style/alt and main padding + margins

--- cgm-analytics/src/app/page.tsx
+++ cgm-analytics/src/app/page.tsx
@@ -3,15 +3,14 @@ import TopNav from './components/TopNav'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 m-2">
 
       <TopNav />
 
-      <div className="relative flex place-items-center before:absolute before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+      <div className="relative scale-150">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/berkeley-bear.svg"
-          alt="Next.js Logo"
+          alt="Berkeley Logo Black"
           width={350}
           height={37}
           priority
