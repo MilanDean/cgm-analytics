0f655269630ce5efd10ee6a9c5655c7febebefbb
Milan Dean <milan387@gmail.com>
2023-06-10 16:22:19 -0500

    added backend json handling to test returning the CSV file as a table to the user

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,14 +1,22 @@
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, UploadFile
+from fastapi.responses import JSONResponse
+import pandas as pd
 
 app = FastAPI(docs_url="/docs", redoc_url="/redoc", openapi_url="/openapi.json")
 
-
-@app.get("/hello")
-async def table():
-    pass
 
 @app.get("/")
 async def root():
     # 501 error is the default `Not Implemented` status code
     raise HTTPException(status_code=501, detail="Not implemented")
+
+
+@app.post("/process-file")
+async def process_csv_file(file: UploadFile):
+    try:
+        df = pd.read_csv(file.file)
+        # Return the modified data as JSON
+        return JSONResponse(content=df.to_json(orient='records'))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
