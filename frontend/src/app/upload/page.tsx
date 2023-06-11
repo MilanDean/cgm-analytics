8ac0a34bb18c64cfc9fb0996b9d61c7ee395b2d2
Milan Dean <milan387@gmail.com>
2023-06-10 21:48:15 -0500

    Added onclick and onchange to handle the file upload on the /upload route

--- frontend/src/app/upload/page.tsx
+++ frontend/src/app/upload/page.tsx
@@ -1,13 +1,53 @@
-import Image from 'next/image'
-import TopNav from '../components/TopNav'
+'use client';
+import { ChangeEvent, useState } from 'react';
+import axios from 'axios';
+import Image from 'next/image';
+import TopNav from '../components/TopNav';
 
 export default function Upload() {
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!file) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file, file.name);
+
+    try {
+      // Send the file to the FastAPI backend for processing
+      const response = await axios.post('http://127.0.0.1:8000/api/analysis', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0];
+
+    // Validate file type
+    if (selectedFile && selectedFile.type === 'text/csv') {
+      setFile(selectedFile);
+    } else {
+      setFile(null);
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12 m-2">
 
       <TopNav />
       <div className="flex flex-col items-center justify-center">
-        <form className="w-full max-w-lg p-8 rounded-lg shadow-lg bg-transparent border-2 border-black">
+        <form className="w-full max-w-lg p-8 rounded-lg shadow-lg bg-transparent border-2 border-black" onSubmit={handleSubmit}>
 
           <div className="p-6 mb-4">
             <label htmlFor="fileInput" className="text-black font-bold">
@@ -16,7 +56,9 @@ export default function Upload() {
             <input
               type="file"
               id="fileInput"
+              accept=".csv"
               className="w-full border border-gray-300 p-2 rounded text-black bg-transparent"
+              onChange={handleFileChange}
             />
           </div>
 
