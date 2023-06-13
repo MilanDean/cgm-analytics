451776372e9cc19cd1163b5eec199cc54caca33a
Milan Dean <milan387@gmail.com>
2023-06-12 20:45:51 -0500

    removed website stuff

--- cgm-analytics/src/app/team/page.tsx
+++
@@ -1,9 +0,0 @@
-import TopNav from "../components/TopNav";
-
-export default function Team() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-12 m-2">
-            <TopNav />
-        </main>
-    );
-}
