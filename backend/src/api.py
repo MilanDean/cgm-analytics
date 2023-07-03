d68917f930ccd9ce781aaca0e6d89392ac4470a1
Milan Dean <milan387@gmail.com>
2023-07-03 13:38:27 -0500

    ran black to format

--- backend/src/api.py
+++ backend/src/api.py
@@ -17,12 +17,14 @@ import time
 
 
 s3 = boto3.client("s3", region_name="us-east-1")
-glue = boto3.client('glue', region_name='us-east-1')
+glue = boto3.client("glue", region_name="us-east-1")
 app = FastAPI(docs_url="/docs", redoc_url="/redoc", openapi_url="/openapi.json")
-conn = connect(aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-               aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-               s3_staging_dir='s3://cgm-analytics-ucb/',
-               region_name='us-east-1')
+conn = connect(
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    s3_staging_dir="s3://cgm-analytics-ucb/",
+    region_name="us-east-1",
+)
 
 # Configure CORS
 app.add_middleware(
@@ -35,15 +37,14 @@ app.add_middleware(
 
 
 def generate_plot(df, x_column=None, y_column=None, filename=None):
-
     font_path = font_manager.findfont(font_manager.FontProperties(family="Arial"))
     plt.rcParams["font.family"] = font_manager.get_font(font_path).family_name
 
-    df['timestamp'] = pd.to_datetime(df['time'])
+    df["timestamp"] = pd.to_datetime(df["time"])
 
-    plt.figure(figsize = (15,5))
-    sns.lineplot(data=df, x=x_column, y=y_column, legend='brief', label=y_column)
-    plt.title('Raw CGM Data')
+    plt.figure(figsize=(15, 5))
+    sns.lineplot(data=df, x=x_column, y=y_column, legend="brief", label=y_column)
+    plt.title("Raw CGM Data")
     plt.tight_layout()
 
     plt.savefig(filename)
@@ -66,7 +67,7 @@ def upload_file_to_s3(bucket_name, file_name, file_data):
         # Check if file_data is a path (str) or a file-like object
         if isinstance(file_data, str):
             # It's a path, so open the file
-            with open(file_data, 'rb') as f:
+            with open(file_data, "rb") as f:
                 s3.upload_fileobj(f, bucket_name, file_name)
         else:
             # It's a file-like object, so ensure we're at the start before uploading
@@ -75,7 +76,9 @@ def upload_file_to_s3(bucket_name, file_name, file_data):
         print(f"File {file_name} uploaded successfully to bucket {bucket_name}")
         return True
     except ClientError as e:
-        print(f"An error occurred while uploading {file_name} to {bucket_name}. Error: {e}")
+        print(
+            f"An error occurred while uploading {file_name} to {bucket_name}. Error: {e}"
+        )
         return False
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
@@ -89,13 +92,12 @@ def check_table_exists(database: str, table: str):
         return True
     except glue.exceptions.EntityNotFoundException:
         return False
-    
+
 
 def check_crawler_status(crawler_name):
-
-    glue = boto3.client('glue', region_name='us-east-1')
+    glue = boto3.client("glue", region_name="us-east-1")
     response = glue.get_crawler(Name=crawler_name)
-    status = response['Crawler']['State']
+    status = response["Crawler"]["State"]
 
     return status
 
@@ -113,7 +115,7 @@ async def process_csv_file(file: UploadFile):
 
     except ClientError as e:
         # If the file doesn't exist, upload it
-        if e.response['Error']['Code'] == '404':
+        if e.response["Error"]["Code"] == "404":
             print("Data not in S3 bucket - Uploading...")
             file.file.seek(0)
             upload_file_to_s3(bucket_name, file.filename, file.file)
@@ -127,14 +129,15 @@ async def process_csv_file(file: UploadFile):
         # Refresh Athena table to run AWS Glue crawler only if the table doesn't exist
         counter = 0
         if not check_table_exists("cgm-source-database", "cgm_analytics_ucb"):
-            glue.start_crawler(Name='cgm-analytics-crawler')
-            while check_crawler_status('cgm-analytics-crawler') != 'READY' or counter < 20:
+            glue.start_crawler(Name="cgm-analytics-crawler")
+            while (
+                check_crawler_status("cgm-analytics-crawler") != "READY" or counter < 20
+            ):
                 print("Waiting for crawler to finish determining data schema...")
                 counter += 1
                 time.sleep(5)
 
         return ProcessedDataResponse(message="Success", file=file.filename)
-
 
 
 def check_if_file_exists(bucket: str, key: str):
@@ -175,14 +178,16 @@ async def get_csv_file(file_name: str):
 
 @app.get("/api/visualization/{filename}")
 async def get_visualization(filename: str):
-    s3_client = boto3.client('s3', region_name='us-east-1')
+    s3_client = boto3.client("s3", region_name="us-east-1")
 
     # Check if the file exists in S3
     try:
-        s3_client.head_object(Bucket=bucket_name, Key=f'{filename}_line_plot.png')
-        line_plot_url = s3_client.generate_presigned_url('get_object',
-                                    Params={'Bucket': bucket_name, 'Key': f'{filename}_line_plot.png'},
-                                    ExpiresIn=3600)
+        s3_client.head_object(Bucket=bucket_name, Key=f"{filename}_line_plot.png")
+        line_plot_url = s3_client.generate_presigned_url(
+            "get_object",
+            Params={"Bucket": bucket_name, "Key": f"{filename}_line_plot.png"},
+            ExpiresIn=3600,
+        )
     except ClientError as e:
         # File does not exist, run the query and generate the graph
         query = """
@@ -196,12 +201,14 @@ async def get_visualization(filename: str):
         columns = [desc[0] for desc in cursor.description]
         df = pd.DataFrame(df, columns=columns)
 
-        generate_plot(df, 'timestamp', 'CGM', 'line_plot.png')
-        upload_file_to_s3(bucket_name, f'{filename}_line_plot.png', 'line_plot.png')
+        generate_plot(df, "timestamp", "CGM", "line_plot.png")
+        upload_file_to_s3(bucket_name, f"{filename}_line_plot.png", "line_plot.png")
 
-        line_plot_url = s3_client.generate_presigned_url('get_object',
-                                    Params={'Bucket': bucket_name, 'Key': f'{filename}_line_plot.png'},
-                                    ExpiresIn=3600)
+        line_plot_url = s3_client.generate_presigned_url(
+            "get_object",
+            Params={"Bucket": bucket_name, "Key": f"{filename}_line_plot.png"},
+            ExpiresIn=3600,
+        )
 
     return {"line_plot_url": line_plot_url}
 
