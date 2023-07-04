11669733318d3e163cf47cb1e9dab0cd45a79759
Milan Dean <milan387@gmail.com>
2023-07-04 02:05:13 -0500

    updated consent button color

--- frontend/src/app/components/UserAgreement.tsx
+++ frontend/src/app/components/UserAgreement.tsx
@@ -59,7 +59,7 @@ export default function UserAgreement({ isOpen, setOpen, setAccepted }: UserAgre
                   </button>
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
+                    className="inline-flex w-full justify-center rounded-md bg-blue-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
                     onClick={() => {
                       setAccepted(true);
                       setOpen(false);
