cbb422b1ac880bf57133b7f8261fd22f76eb1d90
Milan Dean <milan387@gmail.com>
2023-07-27 00:42:01 -0500

    added logo to navbar

--- frontend/src/app/components/TopNav.tsx
+++ frontend/src/app/components/TopNav.tsx
@@ -5,11 +5,15 @@ const TopNav: React.FC = () => (
   <header className="text-gray-600 body-font w-full">
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <Link href="/">
-        <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <span className="ml-3 text-xl">NutriNet</span>
+        <div className="mb-4 md:mb-0">
+            <img
+              className="w-20"
+              src='./HomeLogo.png'
+              alt="Nutrinet"
+            />
         </div>
       </Link>
-      <nav className="md:ml-auto flex flex-wrap px-4 sm:px-6 lg:px-8">
+      <nav className="md:ml-auto flex px-4 sm:px-6 lg:px-8">
         <Link href="/upload">
           <span className="mr-5 hover:text-gray-900">Upload</span>
         </Link>
